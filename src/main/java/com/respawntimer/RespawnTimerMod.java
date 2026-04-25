dc58820a3c021031e9bd5f849ad88558d425c106
fofankochevnik <prostoc48@mail.ru>
2026-04-25 23:45:42 +0300

    Update RespawnTimerMod.java

--- src/main/java/com/respawntimer/RespawnTimerMod.java
+++ src/main/java/com/respawntimer/RespawnTimerMod.java
@@ -5,9 +5,6 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraft.client.Minecraft;
 
 @Mod(RespawnTimerMod.MODID)
@@ -27,25 +24,5 @@ public class RespawnTimerMod {
                 TimerManager.startTimer();
             }
         }
-    }
-
-    @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
-    public void onRenderGui(RenderGuiEvent.Post event) {
-        if (!TimerManager.isActive()) return;
-
-        Minecraft mc = Minecraft.getInstance();
-        if (mc.options.hideGui) return;
-
-        int seconds = TimerManager.getRemainingSeconds();
-        int minutes = seconds / 60;
-        int secs = seconds % 60;
-        String text = String.format("%d:%02d", minutes, secs);
-
-        int color = TimerManager.isWarning() ? 0xFFFF3333 : 0xFFFFFFFF;
-        int textWidth = mc.font.width(text);
-        int screenWidth = event.getGuiGraphics().guiWidth();
-
-        event.getGuiGraphics().drawString(mc.font, text, screenWidth - textWidth - 8, 8, color, true);
     }
 }
